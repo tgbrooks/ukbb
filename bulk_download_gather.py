c7cbacf046d94a868c65dc568e1cdbf7c9466115
Thomas Brooks <thobr@sas.upenn.edu>
2019-08-08 16:42:17 -0400

    Bulk download for field 90001

--- bulk_download_gather.py
+++ bulk_download_gather.py
@@ -7,10 +7,10 @@ DATA_FILE = "data/ukb32828.tab"
 OUTPUT_FILE = "data/bulk_download_list.txt"
 
 # List of columns to extract the raw data locations from 
-DESIRED_FILE_COLUMNS = ["f.90004.0.0"]
+#DESIRED_FILE_COLUMNS = ["f.90004.0.0"]
+DESIRED_FILE_COLUMNS = ["f.90001.0.0"]
 
 data = pandas.read_csv(DATA_FILE, sep="\t", index_col=0)
-
 
 file_columns = [data[column].dropna() for column in DESIRED_FILE_COLUMNS]
 [print(f"Found {len(extracted)} entries in {col}") for extracted, col in zip(file_columns, DESIRED_FILE_COLUMNS)]
