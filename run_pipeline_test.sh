672a816ef862aacb1c0257448a120e139cf0c286
Thomas Brooks <thobr@sas.upenn.edu>
2019-08-15 11:22:45 -0400

    Add run_pipeline_test.sh using tsub.py and status.py
    Seems to manage to execute most of the workflow

---
+++ run_pipeline_test.sh
@@ -0,0 +1,12 @@
+# NOTE: may want to run 'snakemake -n' first to see what is going to be run
+LOGFILE=snakemake.log
+ERRFILE=snakemake.err
+PROJECT=UKBB_FITZGERALD
+MAXJOBS=50
+
+# Can never download more than 10 at a time from UKBB, we'll use less than that to be safe
+MAXDOWNLOADS=5
+
+source venv/bin/activate
+module load java/openjdk-1.8.0
+bsub -P $PROJECT -oo $LOGFILE -eo $ERRFILE snakemake --cluster-config cluster.json --cluster "./tsub.py --timeout 60 '-P $PROJECT -M 10000 -oo {cluster.output} -eo {cluster.error} -J {cluster.name}'" --cluster-status "./status.py" -j $MAXJOBS --resources download=$MAXDOWNLOADS --keep-going --verbose
