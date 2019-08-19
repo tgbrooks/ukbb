e9b1702c6e6bc910999a9509bf2e897035d884bf
Thomas Brooks <tgbrooks@gmail.com>
2019-08-19 16:15:26 -0400

    Greatly improve visualization.py

--- visualize.py
+++ visualize.py
@@ -19,21 +19,24 @@ def visualize(filename):
     #data["time"] = data.index.time
     mask = numpy.ones(data.index.shape).astype(bool)
 
-    grouped = data[mask].groupby("time")
+    grouped = data[mask].groupby(data.index.time)
     day_max = grouped.max()
     day_min = grouped.min()
     day_avg = grouped.mean()
 
     #### Make figures
-    fig = pylab.figure()
-
-    ax1 = fig.add_subplot(211)
     activities = ['sleep', 'sedentary', 'walking', 'tasks-light', 'moderate', 'imputed']
-    ax1.stackplot(data.index, [data[act] for act in activities], labels=activities)
+    activity_colors = ["b", "r", "g", "y", "c", "k"]
 
-    ax2 = fig.add_subplot(212, sharex=ax1)
-    ax2.plot(data['acceleration'])
-    fig.legend()
+    # Long plot
+    #fig = pylab.figure()
+
+    #ax1 = fig.add_subplot(211)
+    #ax1.stackplot(data.index, [data[act] for act in activities], labels=activities)
+
+    #ax2 = fig.add_subplot(212, sharex=ax1)
+    #ax2.plot(data['acceleration'])
+    #fig.legend()
 
     # stacked days, double-plotted
     num_days = math.ceil((data.index[-1] - data.index[0]).total_seconds() / (24*60*60)) + 1
@@ -41,16 +44,33 @@ def visualize(filename):
     for i, ax in enumerate(axes):
         start = pandas.to_datetime(data.index[0].date()) + i * pandas.to_timedelta("1 day")
         end = start + pandas.to_timedelta("2 day")
-        day = data['acceleration'].loc[start:end]
+        day = data.loc[start:end]
         index = (day.index - start).total_seconds() / (60*60)
 
-        #ax = fig.add_subplot(num_days, 1, i+1, sharex=last_ax)
-        ax.plot(index.values, day.values)
-        last_ax = ax
+        # Shade regions based off the inferred activity
+        shading_rects = []
+        for act, color in zip(activities, activity_colors):
+            changes = numpy.diff(numpy.concatenate([[0], (day[act] > 0).astype(int), [0]]))
+            starts, = numpy.where(changes > 0)
+            stops, = numpy.where(changes < 0)
+            for (a, b) in zip(starts, stops):
+                first = index[a]
+                last = index[min(b, len(day.index)-1)]
+                rect = ax.axvspan(first, last, facecolor=color, alpha=0.5)
+            # Grab one rect per activity for legend
+            # only do this for the first day
+            shading_rects.append(rect)
+
+        # Plot acceleration
+        ax.plot(index.values, day.acceleration.values, c='k')
 
         ax.set_yticks([],[])
         ax.set_ylabel(start.strftime("%a"))
         ax.set_ylim(0,data['acceleration'].max())
+        ax.set_xticks([0,6,12,18,24,30,36,42,48])
+
+    print(shading_rects)
+    fig.legend(shading_rects, activities)
 
     # 24Hour compressed data
     fig2 = pylab.figure()
