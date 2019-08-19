93fc1f8fcfad73f169fc93af46c0fc839fe49997
Thomas Brooks <tgbrooks@gmail.com>
2019-08-19 14:38:59 -0400

    Fix updates to visualize.py and util.py

--- util.py
+++ util.py
@@ -4,12 +4,11 @@ import numpy
 def read_actigraphy(file):
     data = pandas.read_csv(file, sep=",", index_col=0, parse_dates=[0])
 
-    data = data.set_index(index)
-    data.rename(columns={data.columns[0]:"acceleration"})# Acceleration column name contains some extra information, ignore it
+    data.rename(columns={data.columns[0]:"acceleration"}, inplace=True)# Acceleration column name contains some extra information, ignore it
 
     # clear the imputed values since they are junk
     is_imputed = data.imputed.astype(bool)
     data.loc[is_imputed] = float("NaN")
-    data[is_imputed, 'imputed'] = 1 # restore the 'imputed' value since we just overwrote it as NaN
+    data.loc[is_imputed, 'imputed'] = 1 # restore the 'imputed' value since we just overwrote it as NaN
 
     return data
--- visualize.py
+++ visualize.py
@@ -8,18 +8,18 @@ import pylab
 import util
 
 def visualize(filename):
-    if filename.endswith("csv"):
+    if filename.endswith("csv") or filename.endswith(".csv.gz"):
         data = util.read_actigraphy(filename)
     else:
         data = pandas.read_csv(filename, sep="\t", index_col=0, parse_dates=[0])
 
-    data_10min = data.resample("10Min").mean()
+    #data = data.resample("10Min").mean()
 
     # Group by time of day and
-    data_10min["time"] = data_10min.index.time
-    mask = numpy.ones(data_10min.index.shape).astype(bool)
+    #data["time"] = data.index.time
+    mask = numpy.ones(data.index.shape).astype(bool)
 
-    grouped = data_10min[mask].groupby("time")
+    grouped = data[mask].groupby("time")
     day_max = grouped.max()
     day_min = grouped.min()
     day_avg = grouped.mean()
@@ -28,18 +28,20 @@ def visualize(filename):
     fig = pylab.figure()
 
     ax1 = fig.add_subplot(211)
-    ax1.stackplot(data_10min.index, [data_10min.sleeping, data_10min.walking, data_10min.sedentary, data_10min['light-tasks'], data_10min['moderate'])
+    activities = ['sleep', 'sedentary', 'walking', 'tasks-light', 'moderate', 'imputed']
+    ax1.stackplot(data.index, [data[act] for act in activities], labels=activities)
 
     ax2 = fig.add_subplot(212, sharex=ax1)
-    ax2.plot(data_10min['acceleration'])
+    ax2.plot(data['acceleration'])
+    fig.legend()
 
     # stacked days, double-plotted
     num_days = math.ceil((data.index[-1] - data.index[0]).total_seconds() / (24*60*60)) + 1
     fig, axes = pylab.subplots(nrows=num_days, sharex=True)
     for i, ax in enumerate(axes):
-        start = pandas.to_datetime(data_10min.index[0].date()) + i * pandas.to_timedelta("1 day")
+        start = pandas.to_datetime(data.index[0].date()) + i * pandas.to_timedelta("1 day")
         end = start + pandas.to_timedelta("2 day")
-        day = data_10min['acceleration'].loc[start:end]
+        day = data['acceleration'].loc[start:end]
         index = (day.index - start).total_seconds() / (60*60)
 
         #ax = fig.add_subplot(num_days, 1, i+1, sharex=last_ax)
@@ -48,7 +50,7 @@ def visualize(filename):
 
         ax.set_yticks([],[])
         ax.set_ylabel(start.strftime("%a"))
-        ax.set_ylim(0,data_10min['acceleration'].max())
+        ax.set_ylim(0,data['acceleration'].max())
 
     # 24Hour compressed data
     fig2 = pylab.figure()
