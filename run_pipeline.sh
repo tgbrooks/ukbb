f37333886b44cd8bcd83bf00e8c14a4694681921
Thomas Brooks <thobr@sas.upenn.edu>
2019-09-04 13:12:27 -0400

    Remove old 'run_pieline.sh' in favor of the 'test' one

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,11 +1,12 @@
 # NOTE: may want to run 'snakemake -n' first to see what is going to be run
 LOGFILE=snakemake.log
+ERRFILE=snakemake.err
 PROJECT=UKBB_FITZGERALD
-MAXJOBS=5
+MAXJOBS=150
+
 # Can never download more than 10 at a time from UKBB, we'll use less than that to be safe
-MAXDOWNLOADS=5
+MAXDOWNLOADS=9
 
 source venv/bin/activate
 module load java/openjdk-1.8.0
-# NOTE: removed --resources download=$MAXDOWNLOADS for now since having problem submitting jobs
-bsub -P $PROJECT -oo $LOGFILE snakemake --cluster-config cluster.json --cluster "bsub -P $PROJECT -M 10000 -oo {cluster.output} -eo {cluster.error} -J {cluster.name}" -j $MAXJOBS --keep-going
+bsub -P $PROJECT -oo $LOGFILE -eo $ERRFILE snakemake --cluster-config cluster.json --cluster "./tsub.py '-P $PROJECT -M 12000 -oo {cluster.output} -eo {cluster.error} -J {cluster.name}'" --cluster-status "./status.py" -j $MAXJOBS --resources download=$MAXDOWNLOADS --keep-going
