803fc806749e5e2b4fee114a0dd89f6407508a66
Thomas Brooks <thobr@sas.upenn.edu>
2019-09-12 12:23:48 -0400

    Process fields based off data codings

--- datafield_codings.py
+++ datafield_codings.py
@@ -1,37 +1,55 @@
 # Contains information about (some) data codings for UKBiobank Fields
 # selected for codings that we need
 
+# a coding has the following:
+# fields = list of field numbers that indicate which fields are coded by this coding
+# to_nan = list of values that should be treated as missing (NaN) if present in this coding's field
+# fillvalue = value to use when filling in a field of this coding if it was not asked
+#             due to a dependent question being answered in the negative
+#             eg: we fill "Did you have a change in appetite (during worst episode of depression)?" with NO
+#                 if they said they never had an episode of depression
 codings = dict(
 
-    502 = dict(
+    coding9 = dict(
+        #0   Female
+        #1   Male
+        fields = [31],
+        to_nan = [],
+    ),
+
+    coding502 = dict(
         #0  No
         #1  Yes
-        fields = [ 21066, 21068, 20532, 20435, 20541, 20431, 20406, 20401, 20456, 20503, 20468, 20474, 21024, 
+        fields = [ 21066, 21068, 20532, 20435, 20541, 20431, 20406, 20401, 20456, 20503, 20468, 20474, 21024,
                    20421, 20502, 20501, 20463, 20404, 20466, 20471, 20499, 20500, 20477, 20425, 21065, 20449,
                    20450, 21067, 20540, 20428, 20448, 21064, 20542, 20437, 20538],
         to_nan = [-818, -121],
+        fillvalue = 0,
     ),
 
-    503 = dict(
+    coding503 = dict(
         #-818 Prefer not to answer
         #0 No
         #1 Yes
         fields = [ 21027, 20484, 20486, 21035, 21038, 20447, 21074, 20483, 21062, 21063, 20446, 20441,
                     20480, 21073, 21071, 20457, 20504, 20415, 20432, 20481, 21076, ],
-        to_nan = [-818]
+        to_nan = [-818],
+        fillvalue = 0,
     ),
 
-    504 = dict(
+    coding504 = dict(
         #-818    Prefer not to answer
         #1   Not at all
         #2   Several days
         #3   More than half the days
         #4   Nearly every day
          fields = [ 20518, 20505, 20510, 20512, 20507, 20519, 20506, 20509, 20514, 20511, 20516, 20513, 20508, 20515, 20520, 20517, ],
-     to_nan = [-818],
+         to_nan = [-818],
+         type = "ordinal",
+         fillvalue = 1,
     ),
 
-    505 = dict(
+    coding505 = dict(
             #-818    Prefer not to answer
             #-121    Do not know
             #1   Less than half of the day
@@ -40,9 +58,11 @@ codings = dict(
             #4   All day long
             fields = [ 20436, ],
             to_nan = [-121, -818],
+            type = "ordinal",
+            fillvalue = 0,
     ),
 
-    506 = dict(
+    coding506 = dict(
             #-818    Prefer not to answer
             #-121    Do not know
             #1   Less often
@@ -50,9 +70,11 @@ codings = dict(
             #3   Every day
             fields = [20439],
             to_nan = [-818, -121],
+            type = "ordinal",
+            fillvalue = 0,
     ),
 
-    507 = dict(
+    coding507 = dict(
             #-818    Prefer not to answer
             #-121    Do not know
             #0   Stayed about the same or was on a diet
@@ -60,17 +82,20 @@ codings = dict(
             #2   Lost weight
             #3   Both gained and lost some weight during the episode
             fields = [20536],
-            to_nan = [-818, -121]
+            to_nan = [-818, -121],
+            type = "categorical",
+            fillvalue = 0,
     ),
 
-    508 = dict(
+    coding508 = dict(
             #0   No
             #1   Yes
             fields = [ 20534, 20533, 20535],
             to_nan = [],
+            fillvaue = 0,
     ),
 
-    509 = dict(
+    coding509 = dict(
             #-818    Prefer not to answer
             #1   Less than a month
             #2   Between one and three months
@@ -80,17 +105,11 @@ codings = dict(
             #6   Over two years
             fields = [20438],
             to_nan = [-818],
+            type="ordinal",
+            fillvalue = 0,
     ),
 
-    1406 = dict(
-            #1   Talking therapies, such as psychotherapy, counselling, group therapy or CBT
-            #3   Other therapeutic activities such as mindfulness, yoga or art classes
-            #-818    Prefer not to answer
-            fields = [20550, 20547],
-            to_nan = [-818],
-    ),
-
-    510 = dict(
+    coding510 = dict(
             #-818    Prefer not to answer
             #0   Not at all
             #1   A little
@@ -98,24 +117,27 @@ codings = dict(
             #3   A lot
             fields = [20418, 20440],
             to_nan = [-818],
+            fillvalue = 0,
     ),
 
-    511 = dict(
+    coding511 = dict(
             #-818    Prefer not to answer
             #-999    Too many to count / One episode ran into the next
             # or a number (of episodes)
             fields = [20442],
             to_nan = [-818],
             #TOOD: what to do with -999?
+            fillvalue = 0,
     ),
 
-    513 = dict(
+    coding513 = dict(
             # Age in years if answered
             fields = [20433, 20434],
             to_nan = [-818, -121],
+            fillvalue = float("NaN"),
     ),
 
-    514 = dict(
+    coding514 = dict(
             #-818    Prefer not to answer
             #-313    Not applicable
             #-121    Do not know
@@ -123,9 +145,10 @@ codings = dict(
             #1   Yes
             fields = [20445],
             to_nan = [-818, -313, -121],
+            fillvalue = 0,
     ),
 
-    515 = dict(
+    coding515 = dict(
             #-818    Prefer not to answer
             #-121    Do not know
             #1   Less than 24 hours
@@ -133,9 +156,10 @@ codings = dict(
             #3   A week or more
             fields = [20492],
             to_nan = [-818, -121],
+            type="ordinal",
     ),
 
-    516 = dict(
+    coding516 = dict(
             #-818    Prefer not to answer
             #-121    Do not know
             #0   No problems
@@ -144,7 +168,7 @@ codings = dict(
             to_nan = [-818, -121],
     ),
 
-    517 = dict(
+    coding517 = dict(
             #-999    All my life / as long as I can remember
             # or period in months
             fields = [20420],
@@ -152,7 +176,7 @@ codings = dict(
             # TODO: what to do with -999?
     ),
 
-    518 = dict(
+    coding518 = dict(
             #-818    Prefer not to answer
             #-121    Do not know
             #1   One thing
@@ -161,7 +185,7 @@ codings = dict(
             to_nan = [-818, -121],
     ),
 
-    519 = dict(
+    coding519 = dict(
             #-818    Prefer not to answer
             #-121    Do not know
             #0   Never
@@ -170,15 +194,63 @@ codings = dict(
             #3   Often
             fields = [ 20537, 20539],
             to_nan = [-818, -121],
+            type="ordinal",
     ),
 
-    520 = dict(
+    coding520 = dict(
             #-121    Do not know
             #0   No
             #1   Yes
-
             fields = [ 20419, 20429, 20427, 20423, 20422, 20426, 20417, ],
-            to_nan = [-121]
+            to_nan = [-121],
+            fillvalue = 0,
     ),
 
+    coding537 = dict(
+            #-818   Prefer not to answer
+            #-121    Do not know
+            #1   Extremely happy
+            #2   Very happy
+            #3   Moderately happy
+            #4   Moderately unhappy
+            #5   Very unhappy
+            #6   Extremely unhappy
+            fields = [ 20458, 20459],
+            to_nan = [-818, -121],
+            type = "ordinal",
+    ),
+
+    coding538 = dict(
+            #-818    Prefer not to answer
+            #-121    Do not know
+            #1   Not at all
+            #2   A little
+            #3   A moderate amount
+            #4   Very much
+            #5   An extreme amount
+            fields = [20460],
+            to_nan = [-818, -121],
+            type = "ordinal",
+    ),
+
+    coding1405 = dict(
+            #1  Unprescribed medication (more than once)
+            #3   Medication prescribed to you (for at least two weeks)
+            #4   Drugs or alcohol (more than once)
+            #-818    Prefer not to answer
+            fields = [20549, 20546],
+            to_nan = [-818],
+            type="list",
+            fillvalue = "", #TODO: how do we fill in as an empty list?
+    ),
+
+    coding1406 = dict(
+            #1   Talking therapies, such as psychotherapy, counselling, group therapy or CBT
+            #3   Other therapeutic activities such as mindfulness, yoga or art classes
+            #-818    Prefer not to answer
+            fields = [20550, 20547],
+            to_nan = [-818],
+            type = "list",
+            fillvalue = "", #TODO: how do we fill in as an empty list?
+    ),
 )
--- gather_depression_fields.py
+++ gather_depression_fields.py
@@ -4,6 +4,9 @@ import argparse
 
 import pandas
 
+import fields_of_interest
+import datafield_codings
+
 parser = argparse.ArgumentParser(description="Generate a smaller table of the variables we care about")
 parser.add_argument("-t", "--tsv", help="path to file of tab-separated data on the subjects", nargs="+")
 parser.add_argument("-o", "--output", help="path to output to")
@@ -14,125 +17,72 @@ data_tables = [pandas.read_csv(path, sep="\t", index_col=0)
                 for path in args.tsv]
 data = pandas.concat(data_tables, axis=1)
 
-### Data about which fields to take
-general_fields = dict(
-    actigraphy_file = 90004,
-    birth_year = 34,
-    sex = 31,
-) 
-
-mental_health_fields = dict(
-    date_of_mental_health_questionnaire = 20400,
-    ever_prolonged_depression = 20446,
-    ever_prolonged_loss_of_interest = 20441,
-    ever_worried_much_more = 20425,
-    longest_period_worried = 20420,
-    sought_profession_help_mental_distress = 20499,
-    ever_mental_distress_prevent_activities = 20500,
-    mental_health_problems_diagnosed = 20544,
-    general_happiness = 20458,
-    happiness_with_health = 20459,
-    belief_life_meaningful = 20460,
-)
-
-# Depends upon ever_worried_much_more and longest_period_worried > 6 months
-anxiety_dependent_fields = dict(
-    ever_felt_worried_more_than_month = 20421, #NOTE: dependent upon 20420 but not 20425
-    activities_to_treat_anxiety = 20550,
-    difficulties_concentrating_worst_anxiety = 20419,
-    difficulty_stopping_worrying_worst_anxiety = 20541,
-    easily_tired_worst_anxiety = 20429,
-    freq_of_difficulty_controlling_worry_worst_anxiety = 20537,
-    freq_of_inability_to_stop_worrying_worst_anxiety = 20539,
-    trouble_sleeping_worst_anxiety = 20427,
-    impact_normal_roles_worst_anxiety = 20418,
-    on_edge_worst_anxiety = 20423,
-    irritable_worst_anxiety = 20422,
-    multiple_worries_worst_anxiety = 20540,
-    number_of_worries_worst_anxiety = 20543,
-    profession_informed_anxiety = 20428,
-    restless_worst_anxiety = 20426,
-    stronger_worrying_anxiety = 20542,
-    substances_taken_anxiety = 20549,
-    tense_sore_aching_worst_anxiety = 20417,
-    worried_most_days_worst_anxiety = 20538,
-)
-
-recent_anxiety = dict(
-    recent_irritability = 20505,
-    recent_foreboding = 20512,
-    recent_anxiety = 20506,
-    recent_uncontrollable_worrying = 20509,
-    recent_restlessness = 20516,
-    recent_trouble_relaxing = 20515,
-    recent_worrying_too_much = 20520,
-)
-
-
-# Fields only asked if ever_prolonged_depression or ever_prolonged_loss_of_interest were True
-depression_dependent_fields = dict(
-    activities_to_treat_depression = 20547,
-    age_at_first_episode = 20433,
-    age_at_last_episode = 20434,
-    depression_related_to_childbirth = 20445,
-    depression_related_to_event = 20447,
-    sleep_change_worst_episode = 20532,
-    difficulty_concentrating_worst_episode = 20435,
-    duration_worst_episode = 20438,
-    tiredness_worst_episode = 20449,
-    worthlessness_worst_episode = 20450,
-    fraction_of_day_worst_epsidode = 20436,
-    frequency_depressed_days_worst_episode = 20439,
-    import_on_normal_roles_worst_episode = 20440,
-    number_depressed_periods = 20442,
-    profession_informed_about_depression = 20448,
-    substances_taken_for_depression = 20546,
-    thoughts_of_death_worst_epsiode = 20437,
-    weight_change_worst_epsiode = 20536,
-)
-
-# These were only asked if sleep_change_worst_episode
-sleep_change_type_fields = dict(
-    sleeping_too_much_worst_episode = 20534,
-    trouble_falling_asleep_worst_episode = 20533,
-    waking_too_early_worst_episode = 20535,
-)
-
-# Fields specifically about 'recent' behavior changes
-# Recall that the questionnaire was given much after the actigraphy
-recent_depression = dict(
-    recent_changes_in_speed = 20518,
-    recent_feelings_of_depression = 20510,
-    recent_feeling_inadequacy = 20507,
-    recent_feeling_tired_low_energy = 20519,
-    recent_lack_of_interest = 20514,
-    recent_poor_appetite_overeating = 20511,
-    recent_thoughts_of_suicide = 20513,
-    recent_trouble_concentrating = 20508,
-    recent_sleep_troubles= 20517,
-)
-
-mania_fields = dict(
-    ever_extreme_irritability = 20502,
-    ever_mania = 20501,
-)
-
-# Dependent upon either of the mania fields
-mania_dependent_fields = dict(
-    longest_period_of_mania = 20492,
-    manifestations_of_mania = 20548, # many categories
-    severity_of_problems_due_to_mania = 20493,
-)
-
-# Collect all the different fields
-field_groups = [general_fields, mental_health_fields, anxiety_dependent_fields, recent_anxiety, depression_dependent_fields, sleep_change_type_fields, recent_depression]
-all_fields = dict()
-for group in field_groups:
-    all_fields.update(group)
-
-invert_fields = {val:key for key, val in fields.items()}
+all_fields = {key:f"f.{num}.0.0" for key, num in fields_of_interest.all_fields.items()}
+invert_fields = {val:key for key, val in all_fields.items()}
 data = data.rename(fields=invert_fields)
 
 # Grab just those fields and output
 small_data = data[list(fields.keys())]
+
+
+def process_fields(data, codings):
+    ''' Operates on 'data' dataframe to apply the codings of the fields in it
+    As well as to do the best we can to fill in missing values, etc.
+    '''
+
+    # Go through every coding and examine the fields that it affects
+    # Then process those by filling in NaN values, processing with the type of the field,
+    # and filling in values for questions that weren't asked
+    processed_fields = []
+    for coding_num, coding in datafield_codings.codings.items():
+        for field_name, field in all_fields:
+            if field in coding['fields']:
+                print(f"Applying coding {coding_num} to field {field_name}:{field_num}")
+                processed_fields.append(field_name)
+
+                type = coding.get("type", None)
+                if type == "list":
+                    print(f"Found a `list` type and its values are:")
+                    print(set(data[field_name]))
+                    # TODO: remove this and instead process the list (into multiple entries?)
+                elif type == "ordinal":
+                    # TODO: do we want to process or mark ordinal columns in some manner?
+                    pass
+                elif type == None:
+                    pass
+                else:
+                    # Might want categorical data codings later, processed as one-hot or similar
+                    raise NotImplementedError
+
+                # Set nan-like values to NaN
+                for value in coding.get('to_nan', []):
+                    data.loc[data[field_name] == value, field_name] = float("NaN")
+
+                # Process field dependencies
+                fillvalue = coding.get('fillvalue', None)
+                if fillvalue is not None:
+                    if field in fields_of_interest.anxiety_dependent_fields:
+                        # Anxiety dependent fields asked only if "ever worried much more" or longest period of worrying was at least 6 months
+                        dependency = data[ever_worried_much_more] | (data[longest_period_worried] >  6)
+                        data.loc[~dependency, field_name] = fillvalue
+
+                    if field in fields_of_interest.depression_dependent_fields:
+                        # depression dependent fields asked only if "ever prolonged depression" or "ever prolonged loss of interest"
+                        dependency = data[ever_prolonged_depression] | data[ever_prolonged_loss_of_interest]
+                        data.loc[~dependency, field_name] = fillvalue
+
+                    if field in fields_of_interest.sleep_change_type_fields:
+                        # Only asked how it changed if sleep change was indicated
+                        dependency = data[sleep_change_worst_episode]
+                        data.loc[~dependency, field_name] = fillvalue
+
+                    if field in fields_of_interset.mania_dependent_fields:
+                        dependency = data[ever_extreme_irritability] | data[ever_mania]
+                        data.loc[~dependency, field_name] = fillvalue
+
+    for field_name, field in all_fields.items():
+        if field_name not in processed_fields:
+            print(f"WARNING: no coding provided for field {field_name}:{field}")
+
+small_data = process_fields(small_data)
 small_data.to_csv(args.output, sep="\t")
