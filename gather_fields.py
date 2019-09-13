37c96d3034b43f92ec10d66a0c896f21d8d252b0
Thomas Brooks <thobr@sas.upenn.edu>
2019-09-13 13:32:07 -0400

    Fix gather_fields.py

--- gather_fields.py
+++ gather_fields.py
@@ -5,6 +5,7 @@ import argparse
 import pandas
 
 import fields_of_interest
+from fields_of_interest import all_fields
 import datafield_codings
 
 parser = argparse.ArgumentParser(description="Generate a smaller table of the variables we care about")
@@ -13,37 +14,41 @@ parser.add_argument("-o", "--output", help="path to output to")
 
 args = parser.parse_args()
 
+processed_all_fields = {key:f"f.{num}.0.0" for key, num in all_fields.items()}
+invert_fields = {val:key for key, val in processed_all_fields.items()}
+
 data_tables = [pandas.read_csv(path, sep="\t", index_col=0)
                 for path in args.tsv]
 data = pandas.concat(data_tables, axis=1)
+del data_tables
 
-all_fields = {key:f"f.{num}.0.0" for key, num in fields_of_interest.all_fields.items()}
-invert_fields = {val:key for key, val in all_fields.items()}
-data = data.rename(fields=invert_fields)
+small_data = data[list(processed_all_fields.values())].copy()
+del data
 
-# Grab just those fields and output
-small_data = data[list(fields.keys())]
+small_data.rename(columns=invert_fields, inplace=True)
 
-
-def process_fields(data, codings):
+def process_fields(data):
     ''' Operates on 'data' dataframe to apply the codings of the fields in it
     As well as to do the best we can to fill in missing values, etc.
     '''
+
+    # Those with a questionnaire filled out
+    # we don't want to use fillvalues on people who haven't answered any questionnaires
+    has_questionnaire = ~data.date_of_mental_health_questionnaire.isna()
 
     # Go through every coding and examine the fields that it affects
     # Then process those by filling in NaN values, processing with the type of the field,
     # and filling in values for questions that weren't asked
     processed_fields = []
     for coding_num, coding in datafield_codings.codings.items():
-        for field_name, field in all_fields:
+        for field_name, field in all_fields.items():
             if field in coding['fields']:
-                print(f"Applying coding {coding_num} to field {field_name}:{field_num}")
+                print(f"Applying coding {coding_num} to field {field_name}:{field}")
                 processed_fields.append(field_name)
 
                 type = coding.get("type", None)
                 if type == "list":
-                    print(f"Found a `list` type and its values are:")
-                    print(set(data[field_name]))
+                    raise NotImplementedError
                     # TODO: remove this and instead process the list (into multiple entries?)
                 elif type == "ordinal":
                     # TODO: do we want to process or mark ordinal columns in some manner?
@@ -59,30 +64,35 @@ def process_fields(data, codings):
                     data.loc[data[field_name] == value, field_name] = float("NaN")
 
                 # Process field dependencies
+                # I.e. we can sometimes fill in a reasonable value for a question
+                #      that was never asked due to the value of another question
+                #      If you say no to ever having a problem, we'll put you down as
+                #      saying no for having each specific type of that problem, for example
                 fillvalue = coding.get('fillvalue', None)
                 if fillvalue is not None:
-                    if field in fields_of_interest.anxiety_dependent_fields:
+                    not_asked = pandas.Series(False, index=data.index)
+                    if field_name in fields_of_interest.anxiety_dependent_fields:
                         # Anxiety dependent fields asked only if "ever worried much more" or longest period of worrying was at least 6 months
-                        dependency = data[ever_worried_much_more] | (data[longest_period_worried] >  6)
-                        data.loc[~dependency, field_name] = fillvalue
+                        not_asked |= (data.ever_worried_much_more == 0) & ((data.ever_felt_worried_more_than_month == 0) | (data.longest_period_worried < 6) | (data.longest_period_worried != -999))
 
-                    if field in fields_of_interest.depression_dependent_fields:
+                    if (field_name in fields_of_interest.depression_dependent_fields
+                            or field_name in fields_of_interest.sleep_change_type_fields):
                         # depression dependent fields asked only if "ever prolonged depression" or "ever prolonged loss of interest"
-                        dependency = data[ever_prolonged_depression] | data[ever_prolonged_loss_of_interest]
-                        data.loc[~dependency, field_name] = fillvalue
+                        not_asked |= (data.ever_prolonged_depression == 0) & (data.ever_prolonged_loss_of_interest == 0)
 
-                    if field in fields_of_interest.sleep_change_type_fields:
+                    if field_name in fields_of_interest.sleep_change_type_fields:
                         # Only asked how it changed if sleep change was indicated
-                        dependency = data[sleep_change_worst_episode]
-                        data.loc[~dependency, field_name] = fillvalue
+                        not_asked |= (data.sleep_change_worst_episode == 0)
 
-                    if field in fields_of_interset.mania_dependent_fields:
-                        dependency = data[ever_extreme_irritability] | data[ever_mania]
-                        data.loc[~dependency, field_name] = fillvalue
+                    if field_name in fields_of_interest.mania_dependent_fields:
+                        not_asked |= (data.ever_extreme_irritability == 0) & (data.ever_mania == 0)
+
+                    data.loc[not_asked, field_name] = fillvalue
+                    print(f"Filling {field_name} with {fillvalue} in {not_asked.sum()} cases")
 
     for field_name, field in all_fields.items():
         if field_name not in processed_fields:
             print(f"WARNING: no coding provided for field {field_name}:{field}")
 
-small_data = process_fields(small_data)
+process_fields(small_data)
 small_data.to_csv(args.output, sep="\t")
