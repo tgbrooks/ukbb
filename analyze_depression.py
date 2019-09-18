87dd68c8a720c5eb361b1076de81d519e2c83f27
Thomas Brooks <thobr@sas.upenn.edu>
2019-09-18 09:10:15 -0400

    Add fields_of_interst.py file
    hadn't added it before, even though was using it!

--- analyze_depression.py
+++ analyze_depression.py
@@ -1,6 +1,7 @@
 import pandas
 import numpy
 import pylab
+import scipy
 from statsmodels.multivariate import cancorr
 from scipy.cluster import hierarchy
 
@@ -35,16 +36,23 @@ data['lifetime_anxiety'] =  (data.ever_worried_much_more == 1) | (data.longest_p
 
 ## Do Univariate correlations on all the variables
 corr = data.corr()
+# Restrict to correlations between mental-health and activity values (not within)
+corr = corr.reindex(labels=ukbb_data.columns, columns=list(activity_features.columns) + list(acc_summary.columns))
 # Put corr into long form, so we can sort for all correlation
 corr['first_var'] = corr.index
 corr_long = corr.melt(id_vars=["first_var"])
 corr_long = corr_long.dropna().sort_values(by="value", ascending=False)
-# Drop those correlations that are redundant (don't need A-vs-B and B-vs-A)
-vars = sorted(set(corr_long.variable))
-first_vars_index = numpy.array([vars.index(var) for var in corr_long.first_var])
-second_vars_index = numpy.array([vars.index(var) for var in corr_long.variable])
-corr_long = corr_long[first_vars_index < second_vars_index]
+corr_long.rename(columns={"variable": "second_var", "value":"correlation"}, inplace=True)
+
+# Compute p-values for these correlations
+# however they N varies for each set of two variables due to NaNs
+def corr_p(row):
+    N = numpy.sum( ~(data[row.first_var].isna() | data[row.second_var].isna()) )
+    t = row.correlation * numpy.sqrt((N - 2) / (1 - row.correlation**2))
+    return scipy.stats.distributions.t(df=N-2).sf(abs(t)) * 2
+corr_long['p'] = corr_long.apply(corr_p, axis=1)
 corr_long.to_csv("../processed/mental_health/univariate_correlations.txt", sep="\t", index=False)
+
 
 def plot_corr(A,B, title='', order=False):
     # Show correlations between two matrices A, B
@@ -85,9 +93,10 @@ def plot_corr(A,B, title='', order=False):
     ax.set_xticklabels(B.columns, rotation=90)#, horizontalalignment="right")
     ax.set_xticks(numpy.arange(len(B.columns)))
     ax.set_yticklabels(A.columns)
-    fig.subplots_adjust(bottom=0.3, left=0.3)
+    fig.subplots_adjust(bottom=0.3, left=0.3, top=0.95, right=0.95)
     ax.set_yticks(numpy.arange(len(A.columns)))
-    ax.imshow(corr, cmap="bwr", vmin=-1, vmax=1)
+    im = ax.imshow(corr, cmap="bwr", vmin=-1, vmax=1)
+    pylab.colorbar(im, ax=ax)
 
     pylab.title(title)
 
@@ -111,22 +120,25 @@ activity = activity.loc[both_index]
 mental_health_order = hierarchy.leaves_list(hierarchy.linkage(mental_health.T, metric="correlation", optimal_ordering=True))
 activity_order = hierarchy.leaves_list(hierarchy.linkage(activity.T, metric="correlation", optimal_ordering=True))
 mental_health = mental_health.iloc[:,mental_health_order]
-activity_order = activity.iloc[:,activity_order]
+activity = activity.iloc[:,activity_order]
 
 # Plot univariate correlations between the two datasets
-plot_corr(mental_health, mental_health)
-pylab.gcf().savefig("../processed/mental_health/mental_health_correlations.svg", format="svg")
-plot_corr(activity, activity)
-pylab.gcf().savefig("../processed/mental_health/activity_correlations.svg", format="svg")
-plot_corr(mental_health, activity)
-pylab.gcf().savefig("../processed/mental_health/activity_mental_health_correlation.svg", format="svg")
+plot_corr(mental_health, mental_health, "Mental Health Correlations")
+pylab.savefig("../processed/mental_health/mental_health_correlations.svg")
+plot_corr(activity, activity, "Activity Correlations")
+pylab.savefig("../processed/mental_health/activity_correlations.svg")
+plot_corr(mental_health, activity, "Activity - Mental Health Correlations")
+pylab.savefig("../processed/mental_health/activity_mental_health_correlation.svg")
 
 
 #### Perform CCA on activity vs mental health questionnaire
 # Perform CCA
 cca_res = cancorr.CanCorr(mental_health, activity)
-print(cca_res.corr_test().summary())
-cca_corr = cca_res.corr_test().stats['Canonical Correlation']
+cca_test = cca_res.corr_test()
+print(cca_test.summary())
+cca_corr = cca_test.stats['Canonical Correlation']
+cca_pvals = cca_test.stats['Pr > F']
+cca_corr = cca_corr[cca_pvals < 0.05]
 
 ## Do permutation test of the CCA results
 def permutation_test(A,B,N = 10_000, num_correlations = 1):
@@ -149,17 +161,17 @@ if PERFORM_PERMUTATION_TEST:
     perm_p, perm_correlations = permutation_test(mental_health, activity, N)
     print(f"Done permutation test.\nTop CCA factor has  p = {perm_p}")
     # When I ran this to N = 4020 I got
-    # p = 0.00025 for the top component
+    # p = 0.00025 for the top component, which is the lowest possible (i.e. no permutations exceeded our correlation)
 
 # Examine the top CCA component
 # Compute the correlations of each univariate variable with the scores
-mental_health_coefficients = pandas.DataFrame(cca_res.y_cancoef, index=mental_health.columns)
-activity_coefficients = pandas.DataFrame(cca_res.x_cancoef, index=activity.columns)
+mental_health_coefficients = pandas.DataFrame(cca_res.y_cancoef[:,cca_pvals < 0.05], index=mental_health.columns)
+activity_coefficients = pandas.DataFrame(cca_res.x_cancoef[:,cca_pvals < 0.05], index=activity.columns)
 mental_health_scores = mental_health @ mental_health_coefficients
 activity_scores = activity @ activity_coefficients
 plot_corr(mental_health_scores, mental_health, "Mental Health Variable Correlations with Canonical Components")
-pylab.gcf().savefig("../processed/mental_health/mental_health_corr_with_top_cca.svg", format="svg")
+pylab.savefig("../processed/mental_health/mental_health_corr_with_top_cca.svg")
 plot_corr(activity_scores, activity, "Activity Variable Correlations with Canonical Components")
-pylab.gcf().savefig("../processed/mental_health/activity_corr_with_top_cca.svg", format="svg")
+pylab.savefig("../processed/mental_health/activity_corr_with_top_cca.svg")
 #plot_corr(mental_health_scores, A, "Mental Health CCA Scores vs Activity Measures") #The cross correlations
 #plot_corr(activity_scores, B, "Activity CCA Scores vs Mental Health Measures")
---
+++ fields_of_interest.py
@@ -0,0 +1,116 @@
+### Data about which fields to take
+general_fields = dict(
+    actigraphy_file = 90004,
+    birth_year = 34,
+    sex = 31,
+)
+
+mental_health_fields = dict(
+    date_of_mental_health_questionnaire = 20400,
+    ever_prolonged_depression = 20446,
+    ever_prolonged_loss_of_interest = 20441,
+    ever_worried_much_more = 20425,
+    ever_felt_worried_more_than_month = 20421,
+    sought_professional_help_mental_distress = 20499,
+    ever_mental_distress_prevent_activities = 20500,
+    #mental_health_problems_diagnosed = 20544, # List variable, currently we don't handle those
+    general_happiness = 20458,
+    happiness_with_health = 20459,
+    belief_life_meaningful = 20460,
+)
+
+# Depends upon ever_worried_much_more and longest_period_worried > 6 months
+anxiety_dependent_fields = dict(
+    longest_period_worried = 20420, #NOTE: dependent upon 20420 but not 20425
+    #activities_to_treat_anxiety = 20550, # List variable, currently we don't handle those
+    difficulties_concentrating_worst_anxiety = 20419,
+    difficulty_stopping_worrying_worst_anxiety = 20541,
+    easily_tired_worst_anxiety = 20429,
+    freq_of_difficulty_controlling_worry_worst_anxiety = 20537,
+    freq_of_inability_to_stop_worrying_worst_anxiety = 20539,
+    trouble_sleeping_worst_anxiety = 20427,
+    impact_normal_roles_worst_anxiety = 20418,
+    on_edge_worst_anxiety = 20423,
+    irritable_worst_anxiety = 20422,
+    multiple_worries_worst_anxiety = 20540,
+    number_of_worries_worst_anxiety = 20543,
+    professional_informed_anxiety = 20428,
+    restless_worst_anxiety = 20426,
+    stronger_worrying_anxiety = 20542,
+    #substances_taken_anxiety = 20549, # List variable, currently we don't handle those
+    tense_sore_aching_worst_anxiety = 20417,
+    worried_most_days_worst_anxiety = 20538,
+)
+
+recent_anxiety = dict(
+    recent_irritability = 20505,
+    recent_foreboding = 20512,
+    recent_anxiety = 20506,
+    recent_uncontrollable_worrying = 20509,
+    recent_restlessness = 20516,
+    recent_trouble_relaxing = 20515,
+    recent_worrying_too_much = 20520,
+)
+
+
+# Fields only asked if ever_prolonged_depression or ever_prolonged_loss_of_interest were True
+depression_dependent_fields = dict(
+    #activities_to_treat_depression = 20547, # List variable, currently we don't handle those
+    age_at_first_episode = 20433,
+    age_at_last_episode = 20434,
+    depression_related_to_childbirth = 20445,
+    depression_related_to_event = 20447,
+    sleep_change_worst_episode = 20532,
+    difficulty_concentrating_worst_episode = 20435,
+    duration_worst_episode = 20438,
+    tiredness_worst_episode = 20449,
+    worthlessness_worst_episode = 20450,
+    fraction_of_day_worst_episode = 20436,
+    frequency_depressed_days_worst_episode = 20439,
+    impact_on_normal_roles_worst_episode = 20440,
+    number_depressed_periods = 20442,
+    professional_informed_about_depression = 20448,
+    #substances_taken_for_depression = 20546, # List variable, currently we don't handle those
+    thoughts_of_death_worst_episode = 20437,
+    #weight_change_worst_episode = 20536, # Categorical variable, currently we don't handle those
+)
+
+# These dependent fields were only asked if sleep_change_worst_episode
+sleep_change_type_fields = dict(
+    sleeping_too_much_worst_episode = 20534,
+    trouble_falling_asleep_worst_episode = 20533,
+    waking_too_early_worst_episode = 20535,
+)
+
+# Fields specifically about 'recent' behavior changes
+# Recall that the questionnaire was given much after the actigraphy
+recent_depression = dict(
+    recent_changes_in_speed = 20518,
+    recent_feelings_of_depression = 20510,
+    recent_feeling_inadequacy = 20507,
+    recent_feeling_tired_low_energy = 20519,
+    recent_lack_of_interest = 20514,
+    recent_poor_appetite_overeating = 20511,
+    recent_thoughts_of_suicide = 20513,
+    recent_trouble_concentrating = 20508,
+    recent_sleep_troubles= 20517,
+)
+
+mania_fields = dict(
+    ever_extreme_irritability = 20502,
+    ever_mania = 20501,
+)
+
+# Dependent upon either of the mania fields
+mania_dependent_fields = dict(
+    longest_period_of_mania = 20492,
+    #manifestations_of_mania = 20548, # many categories TODO: code this variable
+    severity_of_problems_due_to_mania = 20493,
+)
+
+# Collect all the different fields
+field_groups = [general_fields, mental_health_fields, anxiety_dependent_fields, recent_anxiety, depression_dependent_fields, sleep_change_type_fields, recent_depression, mania_fields, mania_dependent_fields]
+all_fields = dict()
+for group in field_groups:
+    # Convert field numbers into the right column name eg "f.90001.0.0"
+    all_fields.update(group)
