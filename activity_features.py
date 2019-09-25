6a3ed5e793338d2f056ab376caef354186617980
Thomas Brooks <tgbrooks@gmail.com>
2019-09-25 10:16:28 -0400

    Fix main sleep calling
    And improve various measures

--- activity_features.py
+++ activity_features.py
@@ -7,6 +7,9 @@ COUNTS_PER_DAY = 24*HOURS_TO_COUNTS
 SLEEP_PERIOD = 8*HOURS_TO_COUNTS
 
 MIN_PERIOD_LENGTH = 10 # 5 minutes of sleep consecutively to count as a sleep period
+JUMP_RATIO = 0.5 # Length of longest gap to jump over as a ratio of the size of the adjacent sleep periods
+RIGHT_JUMP_RATIO = 1.5
+MAX_GAP_LENGTH = 2.0 * HOURS_TO_COUNTS
 GOOD_COUNT_THRESHOLD = 0.75
 SLEEP_THRESHOLD = 0.9 # May not be necessary - 30second intervals seem to generally have sleep=0 or 1
 
@@ -26,7 +29,11 @@ def hours_since_noon(timeseries):
 
 def extract_main_sleep_period(day_data):
     sleep = day_data.sleep
-    sleeping = (sleep.values > SLEEP_THRESHOLD).astype('int')# Binarize
+    # Binarize slepe (it seems to be 0 or 1 already aynway)
+    # and this also converts NaNs to 1, i.e. to sleeping which seems to be a common
+    # mistake of the calling algorithm, to give non-wear time to sleep episodes during the night
+    # (only will matter for computing the main sleep period, not for computing total sleep)
+    sleeping = (~(sleep.values < SLEEP_THRESHOLD)).astype('int')
 
     # Find periods of sleep
     diff = numpy.diff(numpy.concatenate([[0], sleeping, [0]]))
@@ -35,43 +42,73 @@ def extract_main_sleep_period(day_data):
 
 
     # Skip any sleep periods of very short length
-    period_lengths = offset_times - onset_times
-    skip = period_lengths < MIN_PERIOD_LENGTH
-    onset_times = onset_times[~skip]
-    offset_times = offset_times[~skip]
+    #period_lengths = offset_times - onset_times
+    #skip = period_lengths < MIN_PERIOD_LENGTH
+    #onset_times = onset_times[~skip]
+    #offset_times = offset_times[~skip]
 
     if len(onset_times) == 0:
         # Never slept at all
-        return dict(onset=pandas.to_datetime("NaT"), offset=pandas.to_datetime("NaT"), num_wakings=float("NaN"), WASO=float("NaN"), other_duration=float("NaN"), acceleration_during_main_sleep=float("NaN"))
+        return dict(onset=pandas.to_datetime("NaT"), offset=pandas.to_datetime("NaT"), num_wakings=float("NaN"), WASO=float("NaN"), acceleration_during_main_sleep=float("NaN"))
 
-    while(True):
-        # Lengths of each sleep period
-        period_lengths = offset_times - onset_times
+    # Lengths of each sleep period
+    period_lengths = offset_times - onset_times
 
-        # Time between consecutive periods
-        gap_lengths = onset_times[1:] - offset_times[:-1]
+    # Time between consecutive periods
+    gap_lengths = onset_times[1:] - offset_times[:-1]
 
-        # Find the gaps that are less than 1 Hour apart
-        # and where both the sleep periods on either side are at least as long as the gap
-        gaps_to_join = (gap_lengths < HOURS_TO_COUNTS) & (gap_lengths < 2*period_lengths[:-1]) & (gap_lengths < 2*period_lengths[1:])
+    # Join sleep periods by:
+    # iterating to the right (later) from each period, grabbing any period
+    # that is sufficiently close. The total non-sleep time between joined periods
+    # must never be more than MAX_GAP_LENGTH. And the lengths of the periods being
+    # joined together must be large enough compared to the gap
+    gaps_to_join = numpy.zeros(shape=gap_lengths.shape, dtype=bool)
+    for i in range(len(gap_lengths)):
+        joined_period_length = period_lengths[i]
 
-        # Join together all periods whose gaps match the above criteria
-        onset_times = numpy.concatenate([onset_times[:1], onset_times[1:][~gaps_to_join]])
-        offset_times = numpy.concatenate([offset_times[:-1][~gaps_to_join], offset_times[-1:]])
+        furthest_jump = i
+        jump_length = 0
+        for j in range(i+1, len(period_lengths)):
+            # Accumulate the jump length by this gap
+            jump_length += gap_lengths[j-1]
 
-        if not gaps_to_join.any():
-            break
+            if jump_length > MAX_GAP_LENGTH or jump_length >= JUMP_RATIO * joined_period_length:
+                # Searched past the max gap length, so need to stop
+                break
+
+            # Add this period to the lengths of those joined so far
+            # Note: we may not actually end up joining this period
+            # but we need to include its length in the next step
+            joined_period_length += period_lengths[j]
+
+            if jump_length >= RIGHT_JUMP_RATIO * period_lengths[j]:
+                # Too short a period to jump to
+                continue
+
+            # We've accepted the jump to at least this far now
+            furthest_jump = j
+
+        # Join all gaps (possibly 0, possibly many) that span between these two periods
+        gaps_to_join[i:furthest_jump] = True
+
+    # Join together all periods whose gaps match the above criteria
+    onset_times = numpy.concatenate([onset_times[:1], onset_times[1:][~gaps_to_join]])
+    offset_times = numpy.concatenate([offset_times[:-1][~gaps_to_join], offset_times[-1:]])
 
     # Extract main (longest) period
     period_lengths = offset_times - onset_times
     best_period = numpy.argmax(period_lengths)
 
-    onset = sleep.index[0] + pandas.to_timedelta( str(onset_times[best_period] / HOURS_TO_COUNTS) + "H")
-    offset = sleep.index[0] + pandas.to_timedelta( str(offset_times[best_period] / HOURS_TO_COUNTS) + "H")
+    onset_index = onset_times[best_period]
+    offset_index = offset_times[best_period]
 
-    num_wakings = numpy.sum(numpy.diff(numpy.concatenate([[0], sleep[onset:offset] > SLEEP_THRESHOLD, [0]])) < 0)
-    WASO = numpy.sum(1 - sleep[onset:offset]) / HOURS_TO_COUNTS
-    other_duration = (numpy.sum(sleep) - numpy.sum(sleep[onset:offset])) / HOURS_TO_COUNTS
+    onset = sleep.index[0] + pandas.to_timedelta( str(onset_index / HOURS_TO_COUNTS) + "H")
+    offset = sleep.index[0] + pandas.to_timedelta( str(offset_index / HOURS_TO_COUNTS) + "H")
+
+    # Number of times woken up
+    # counted as number of transitions from sleep
+    num_wakings = numpy.sum(numpy.diff(numpy.concatenate([[0], sleeping[onset_index:offset_index] > SLEEP_THRESHOLD, [0]])) < 0) - 1
+    WASO = numpy.sum(1 - sleeping[onset_index:offset_index]) / HOURS_TO_COUNTS
 
     # Average acceleration duing main sleep
     acceleration_during_main_sleep = day_data.acceleration[onset_times[best_period]:offset_times[best_period]].mean()
@@ -80,7 +117,6 @@ def extract_main_sleep_period(day_data):
                 offset=offset,
                 num_wakings=num_wakings,
                 WASO=WASO,
-                other_duration=other_duration,
                 acceleration_during_main_sleep=acceleration_during_main_sleep)
 
 def RA(activity):
@@ -154,18 +190,41 @@ def activity_features(data):
         by_day = data.resample("1D", base=0.5)
         main_sleep_period = by_day.apply(extract_main_sleep_period)
         main_sleep_period_df = pandas.DataFrame(main_sleep_period.tolist(), index=main_sleep_period.index)
-        main_sleep_onset, main_sleep_offset, main_sleep_wakings, main_sleep_WASO, other_sleep_duration, acceleration_during_main_sleep = [main_sleep_period_df[col].copy() for col in main_sleep_period_df.columns]
+        main_sleep_onset, main_sleep_offset, main_sleep_wakings, main_sleep_WASO, acceleration_during_main_sleep = [main_sleep_period_df[col].copy() for col in main_sleep_period_df.columns]
 
         main_sleep_duration = (main_sleep_offset - main_sleep_onset).dt.total_seconds()/60/60
         main_sleep_ratio = (main_sleep_duration - main_sleep_WASO)/ main_sleep_duration
+
+        # Define sleep as either 'main sleep' or 'other sleep'
+        data['main_sleep_period'] = False
+        for day in main_sleep_onset.index:
+            if pandas.isna(main_sleep_onset[day]):
+                continue
+            data.loc[main_sleep_onset[day]:main_sleep_offset[day], 'main_sleep_period'] = True
+        data['main_sleep'] = (data.sleep > 0) & data.main_sleep_period
+        data['other_sleep'] = (data.sleep > 0) & (~data.main_sleep_period)
+        # For computing 'other' times, we want the data to be for a midnight-to-midnight day
+        # not the noon-to-noon day that we use for sleep periods
+        by_midnight_day = data.resample("1D", base=0.0)
+
+        # Collect day-level results
+        results_by_day = main_sleep_period_df
+        results_by_day['main_sleep_duration'] = by_day.main_sleep.sum() / HOURS_TO_COUNTS
+        results_by_day['other_sleep'] = (by_midnight_day.other_sleep.sum() / HOURS_TO_COUNTS).values
+        # Total sleep is napping between that days midnight-midnight period
+        # plus actual sleep in the main sleep from the noon-noon period
+        results_by_day['total_sleep'] = results_by_day.main_sleep_duration - results_by_day.WASO + results_by_day.other_sleep
+        results_by_day['main_sleep_ratio'] = (results_by_day.main_sleep_duration - results_by_day.WASO) / results_by_day.main_sleep_duration
 
         # Throw out days without nearly all of the hours
         # eg: if the data starts at Monday 10:00am, we don't want to consider Sunday noon - Monday noon a day
         # should have 2880 for a complete day
         days_invalid = (by_day.sleep.count() < 2500)
-        for measure in [main_sleep_onset, main_sleep_offset, main_sleep_wakings, main_sleep_WASO, main_sleep_duration, main_sleep_ratio, other_sleep_duration]:
+        for measure in [main_sleep_onset, main_sleep_offset, main_sleep_wakings, main_sleep_WASO, main_sleep_duration, main_sleep_ratio, acceleration_during_main_sleep]:
             measure[days_invalid] = float("NaN")
 
+
+        # Collect summary level results, across all days
         results.update(dict(
             # hours past 0:00 AM (usually of the day prior to the peak sleep)
             sleep_peak_time_avg = hours_since_noon(sleep_peak_time).mean() + 12,
@@ -194,25 +253,13 @@ def activity_features(data):
             main_sleep_ratio_std = main_sleep_ratio.std(),
 
             # Time spent sleeping NOT in main sleep period
-            other_sleep_duration_avg = other_sleep_duration.mean(),
-            other_sleep_duration_std = other_sleep_duration.std(),
+            other_sleep_duration_avg = by_midnight_day.other_sleep.sum().mean() / HOURS_TO_COUNTS,
+            other_sleep_duration_std = by_midnight_day.other_sleep.sum().std() / HOURS_TO_COUNTS,
 
             #Average acceleration during the main sleep period
             acceleration_during_main_sleep_avg = acceleration_during_main_sleep.mean(),
             acceleration_during_main_sleep_std = acceleration_during_main_sleep.std()
         ))
-
-        by_day = dict(
-            total_sleep = by_day.sleep.sum() * 30 / 60 / 60,
-            main_sleep_onset = main_sleep_onset,
-            main_sleep_offset = main_sleep_offset,
-            main_sleep_wakings = main_sleep_wakings,
-            main_sleep_WASO = main_sleep_WASO,
-            other_sleep_duration = other_sleep_duration,
-            main_sleep_duration = main_sleep_duration,
-            main_sleep_ratio = main_sleep_ratio,
-            acceleration_during_main_sleep = acceleration_during_main_sleep,
-        )
 
     # Add RA/IS/IV values for each measure
     for activity in ['sleep', 'walking', 'sedentary', 'moderate', 'acceleration', 'tasks-light', 'MET', 'temp', 'light']:
@@ -248,7 +295,7 @@ def activity_features(data):
 
     #daylight_threshold = (data.light.max() - data.light.min()) / 10 + data.light.min()
 
-    return results, by_day
+    return results, results_by_day
 
 def run(input, output):
     '''Compute and output activity features'''
