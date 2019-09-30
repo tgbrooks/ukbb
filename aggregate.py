bdaf59bd12eb4087160d6d71baf79002fec3855f
Thomas Brooks <thobr@sas.upenn.edu>
2019-09-30 15:32:20 -0400

    Aggregate TSV files as well

--- aggregate.py
+++ aggregate.py
@@ -11,17 +11,31 @@ import pandas
 import pathlib
 import json
 
-# Gather all the summary json file data
-data = {}
-for input_path in pathlib.Path(args.input_dir).glob("*" + args.file_suffix):
-    ID = input_path.name[:-len(args.file_suffix)]
-    with open(input_path) as input_file:
-        data[ID] = json.load(input_file)
-print(f"Loaded {len(data)} accelerometer summaries")
+# Gather all the files together into a dict ID -> data
+if args.file_suffix.endswith(".json"):
+    data = {}
+    for input_path in pathlib.Path(args.input_dir).glob("*" + args.file_suffix):
+        ID = input_path.name[:-len(args.file_suffix)]
+        with open(input_path) as input_file:
+            data[ID] = json.load(input_file)
+    print(f"Loaded {len(data)} accelerometer summaries")
 
-# Aggregate the data from many dicts into one dataframe
-# filling in NaNs for any values that are in one dict and not another
-aggregate = pandas.DataFrame.from_dict(data, orient="index")
+    # Aggregate the data from many dicts into one dataframe
+    # filling in NaNs for any values that are in one dict and not another
+    aggregate = pandas.DataFrame.from_dict(data, orient="index")
+
+else:
+    # Aggregate from tab-separated files instead
+    data = []
+    for input_path in pathlib.Path(args.input_dir).glob("*" + args.file_suffix):
+        ID = input_path.name[:-len(args.file_suffix)]
+        id_data = pandas.read_csv(input_path, sep="\t", index_col=0)
+        # Add the ID as a column
+        id_data["ID"] = ID
+        data.append(id_data)
+
+    # Now concatenate all the ID-specific dataframe together
+    aggregate = pandas.concat(data)
 
 # Replace dashes with underscores for python convenience
 aggregate.rename(columns=lambda s: s.replace('-', '_'))
