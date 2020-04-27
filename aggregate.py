d1e37382a558e554d8c5bb0cae4837951d0e641c
Thomas Brooks <thobr@sas.upenn.edu>
2020-04-27 16:31:09 -0400

    Support aggregating seasonal repeat summary files

--- aggregate.py
+++ aggregate.py
@@ -3,7 +3,7 @@ import argparse
 parser = argparse.ArgumentParser(description="Aggregate the summary output of each participant into a tab-separated spreadsheet")
 parser.add_argument("input_dir", help="path to input directory of participant accelerometer analyses")
 parser.add_argument("output_path", help="path to the target tab-separated output spreadsheet of aggregated summary data.")
-parser.add_argument("--file_suffix", help="suffix of all files you want to aggregate", default="_90001_0_0-summary.json")
+parser.add_argument("--file_suffix", help="suffix of all files you want to aggregate", default=["_90001_0_0-summary.json"], nargs="+")
 
 args = parser.parse_args()
 
@@ -12,12 +12,19 @@ import pathlib
 import json
 
 # Gather all the files together into a dict ID -> data
-if args.file_suffix.endswith(".json"):
+suffix = args.file_suffix[0]
+if suffix.endswith(".json"):
     data = {}
-    for input_path in pathlib.Path(args.input_dir).glob("*" + args.file_suffix):
-        ID = input_path.name[:-len(args.file_suffix)]
-        with open(input_path) as input_file:
-            data[ID] = json.load(input_file)
+    for file_suffix in args.file_suffix:
+        for input_path in pathlib.Path(args.input_dir).glob("*" + file_suffix):
+            ID = input_path.name[:-len(file_suffix)]
+            try:
+                with open(input_path) as input_file:
+                    data[ID] = json.load(input_file)
+            except Exception as e:
+                print(f"Error with file {input_path}. SKIPPING")
+                print(e)
+                continue
     print(f"Loaded {len(data)} accelerometer summaries")
 
     # Aggregate the data from many dicts into one dataframe
@@ -27,12 +34,13 @@ if args.file_suffix.endswith(".json"):
 else:
     # Aggregate from tab-separated files instead
     data = []
-    for input_path in pathlib.Path(args.input_dir).glob("*" + args.file_suffix):
-        ID = input_path.name[:-len(args.file_suffix)]
-        id_data = pandas.read_csv(input_path, sep="\t", index_col=0)
-        # Add the ID as a column
-        id_data["ID"] = ID
-        data.append(id_data)
+    for file_suffix in args.file_suffix:
+        for input_path in pathlib.Path(args.input_dir).glob("*" + file_suffix):
+            ID = input_path.name[:-len(file_suffix)]
+            id_data = pandas.read_csv(input_path, sep="\t", index_col=0)
+            # Add the ID as a column
+            id_data["ID"] = ID
+            data.append(id_data)
 
     # Now concatenate all the ID-specific dataframe together
     aggregate = pandas.concat(data)
