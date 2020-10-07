ae0e2230e11c55a8631f3e2c73aa8dc7b6444a41
Thomas Brooks <tgbrooks@gmail.com>
2020-10-07 14:02:40 -0400

    Add instancing

--- process_ukbb_table.py
+++ process_ukbb_table.py
@@ -68,7 +68,8 @@ if args.filter is not None:
     data.dropna(subset=[f"f.{args.filter}.0.0"], inplace=True)
 
 
-output = {}
+base_output = {}
+repeat_output = {}
 for field_name, field in field_group.items():
 
     field_description = fields.loc[field]
@@ -81,129 +82,141 @@ for field_name, field in field_group.items():
     entries = range(field_description.Array)
     instances = range(field_description.Instances)
 
-    # TODO: handle instances
-    if len(instances) > 1:
-        print(f"WARNING: field {field_name}:{field} has {instances} instances but we are only extracting the first.")
+    for instance in range(instances):
+        output = {}
+        type = field_description.ValueType
+        if type in ["Integer", "Date", "Text", "Continuous", "Time"]:
+            dtype = {"Integer": float, "Date":str, "Text":str, "Continuous":float, "Time":str}[type]
 
-    type = field_description.ValueType
-    if type in ["Integer", "Date", "Text", "Continuous", "Time"]:
-        dtype = {"Integer": float, "Date":str, "Text":str, "Continuous":float, "Time":str}[type]
+            start = 0
+            if f"f.{field}.{instance}.0" not in data:
+                # Sometimes the UKBB starts indexing at 1 instead of 0!!
+                # So we detect that here
+                start = 1
 
-        start = 0
-        if f"f.{field}.0.0" not in data:
-            # Sometimes the UKBB starts indexing at 1 instead of 0!!
-            # So we detect that here
-            start = 1
+            for index in entries:
 
-        for index in entries:
+                try:
+                    col = data[f"f.{field}.{instance}.{index + start}"].copy()
+                except KeyError:
+                    print(f"WARNING: failed to find field {field_name}:{field}.{index} in data. Skipping.")
+                    continue
 
+                if coding is not None:
+                    categories = coding.Value
+                    labels = coding.Meaning
+                    for category, label in zip(categories, labels):
+                        if label in NAN_VALUES:
+                            col[col == dtype(category)] = float("NaN")
+                        else:
+                            if index == 0:
+                                print(f"WARNING: Unknown coding '{category}:{label}' in {field_name}:{field} of type {type}")
+
+                col = process_field(field, col)
+
+                if len(entries) > 1:
+                    output[f"{field_name}_V{index}"]  = col
+                else:
+                    output[f"{field_name}"]  = col
+
+        elif type == "Compound":
+            raise NotImplementedError(f"field {field_name}:{field} has ValueType Compound which is not supported")
+        elif type == "Categorical single":
             try:
-                col = data[f"f.{field}.0.{index + start}"].copy()
+                col = data[f"f.{field}.{instance}.0"]
             except KeyError:
-                print(f"WARNING: failed to find field {field_name}:{field}.{index} in data. Skipping.")
+                print(f"WARNING: failed to find field {field_name}:{field} in data. Skipping.")
                 continue
-
-            if coding is not None:
-                categories = coding.Value
-                labels = coding.Meaning
-                for category, label in zip(categories, labels):
-                    if label in NAN_VALUES:
-                        col[col == dtype(category)] = float("NaN")
-                    else:
-                        if index == 0:
-                            print(f"WARNING: Unknown coding '{category}:{label}' in {field_name}:{field} of type {type}")
 
             col = process_field(field, col)
 
-            if len(entries) > 1:
-                output[f"{field_name}_V{index}"]  = col
+            if field_description.Coding == 19: # ICD10-coded field, such as field 40001
+                # We want to not process this since it is not encoded in integer values
+                # but actually is already in the format we want (like "A00")
+                print(f"Identified field {field} as ICD10-encoded - no processing")
+                output[field_name] = col
+                continue
+
+            try:
+                categories = coding.Value.astype(int)
+            except ValueError as e:
+                print(f"WARNING: Skipping field {field} due to non-integer code values")
+                print(e)
+                continue
+
+            labels = coding.Meaning
+            if len(set(labels)) == len(labels):
+                column = pandas.Categorical(data[f"f.{field}.{instance}.0"], categories)
+                # All labels are unique so can just reassign the categories
+                output[field_name] =  column.rename_categories({cat:label for cat, label in zip(categories, labels)})
             else:
-                output[f"{field_name}"]  = col
-
-    elif type == "Compound":
-        raise NotImplementedError(f"field {field_name}:{field} has ValueType Compound which is not supported")
-    elif type == "Categorical single":
-        try:
-            col = data[f"f.{field}.0.0"]
-        except KeyError:
-            print(f"WARNING: failed to find field {field_name}:{field} in data. Skipping.")
-            continue
-
-        col = process_field(field, col)
-
-        if field_description.Coding == 19: # ICD10-coded field, such as field 40001
-            # We want to not process this since it is not encoded in integer values
-            # but actually is already in the format we want (like "A00")
-            print(f"Identified field {field} as ICD10-encoded - no processing")
-            output[field_name] = col
-            continue
-
-        try:
-            categories = coding.Value.astype(int)
-        except ValueError as e:
-            print(f"WARNING: Skipping field {field} due to non-integer code values")
-            print(e)
-            continue
-
-        labels = coding.Meaning
-        if len(set(labels)) == len(labels):
-            column = pandas.Categorical(data[f"f.{field}.0.0"], categories)
-            # All labels are unique so can just reassign the categories
-            output[field_name] =  column.rename_categories({cat:label for cat, label in zip(categories, labels)})
-        else:
-            # Otherwise, some labels have multiple encodings (eg: job categories in coding 2)
-            # so we do the slow thing and map them to strings
-            column = data[f"f.{field}.0.0"]
-            mapping = {float(cat): label for cat, label in zip(categories, labels)}
-            column = column.map(mapping)
-            output[field_name] = column
-    elif type == "Categorical multiple":
-        # The UKBB stores "Category Multiple" variables as multiple columns and each participant filling out
-        # the first several of those columns with the values of the category that they have
-        # Eg you might have
-        # 1 2 Nan
-        # 2 3 4
-        # 5 Nan Nan
-        # to indicate that first person has 1,2, the second had 2,3,4, and third 5.
-        # But this is inconvenient so we swap to making one column per possible response
-        # and giving True or False values to each person for each possibble category
-        # But this is bad for very large categories like the job selection
-        categories = coding.Value
-        if len(categories) > 30:
-            print(f"WARNING: field {field_name}:{field} has more than 30 categories. Skipping")
-            continue
-        labels = coding.Meaning
-        labels = [label.replace(" ", "_") for label in labels]
-        columns = {f"{field_name}_{label}": pandas.Series(float("NaN"),index=data.index)
-                    for label in labels}
-
-        start = 0
-        if f"f.{field}.0.0" not in data:
-            # Sometimes the UKBB starts indexing at 1 instead of 0!!
-            # So we detect that here
-            start = 1
-
-        for index in entries:
-            try:
-                col = data[f"f.{field}.0.{index+start}"]
-            except KeyError:
-                print(f"WARNING: failed to find field {field_name}:{field}.{index} in data. Skipping.")
+                # Otherwise, some labels have multiple encodings (eg: job categories in coding 2)
+                # so we do the slow thing and map them to strings
+                column = data[f"f.{field}.{instance}.0"]
+                mapping = {float(cat): label for cat, label in zip(categories, labels)}
+                column = column.map(mapping)
+                output[field_name] = column
+        elif type == "Categorical multiple":
+            # The UKBB stores "Category Multiple" variables as multiple columns and each participant filling out
+            # the first several of those columns with the values of the category that they have
+            # Eg you might have
+            # 1 2 Nan
+            # 2 3 4
+            # 5 Nan Nan
+            # to indicate that first person has 1,2, the second had 2,3,4, and third 5.
+            # But this is inconvenient so we swap to making one column per possible response
+            # and giving True or False values to each person for each possibble category
+            # But this is bad for very large categories like the job selection
+            categories = coding.Value
+            if len(categories) > 30:
+                print(f"WARNING: field {field_name}:{field} has more than 30 categories. Skipping")
                 continue
+            labels = coding.Meaning
+            labels = [label.replace(" ", "_") for label in labels]
+            columns = {f"{field_name}_{label}": pandas.Series(float("NaN"),index=data.index)
+                        for label in labels}
 
-            col = process_field(field, col)
+            start = 0
+            if f"f.{field}.{instance}.0" not in data:
+                # Sometimes the UKBB starts indexing at 1 instead of 0!!
+                # So we detect that here
+                start = 1
 
-            for value, label in zip(categories, labels):
-                if index == 0:
-                    valid = ~col.isna()
-                    columns[f"{field_name}_{label}"][valid] = 0
+            for index in entries:
+                try:
+                    col = data[f"f.{field}.{instance}.{index+start}"]
+                except KeyError:
+                    print(f"WARNING: failed to find field {field_name}:{field}.{index} in data. Skipping.")
+                    continue
 
-                columns[f"{field_name}_{label}"][col == int(value)] = 1
-        output.update(columns)
-    else:
-        raise NotImplementedError(f"field {field_name}:{field} has unown type {type}")
+                col = process_field(field, col)
 
-output = pandas.DataFrame(output)
+                for value, label in zip(categories, labels):
+                    if index == 0:
+                        valid = ~col.isna()
+                        columns[f"{field_name}_{label}"][valid] = 0
+
+                    columns[f"{field_name}_{label}"][col == int(value)] = 1
+            output.update(columns)
+        else:
+            raise NotImplementedError(f"field {field_name}:{field} has unown type {type}")
+
+        if instance == 0:
+            base_output.update(output)
+        else:
+            instance_output = {key + f"_V{instance}":value for key, value in output.items()}
+            repeat_output.update(instance_output)
+
+# Output the base (first repeat) data
+output = pandas.DataFrame(base_output)
 if args.output.endswith("txt"):
     output.to_csv(args.output, sep="\t")
 elif args.output.endswith("h5"):
     output.to_hdf(args.output, key="data", sep="\t", mode="w", format="table")
+
+# Output the repeated instances of the data
+repeat_output = pandas.DataFrame(repeat_output)
+if args.output.endswith("txt"):
+    output.to_csv("repeat." + args.output, sep="\t")
+elif args.output.endswith("h5"):
+    output.to_hdf("repeat." + args.output, key="data", sep="\t", mode="w", format="table")
