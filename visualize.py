cb2a37cb6ec126e3670703ab2867d427e7ee6d49
Thomas Brooks <tgbrooks@gmail.com>
2022-03-28 10:57:42 -0400

    Add temperature trace mode

--- visualize.py
+++ visualize.py
@@ -15,18 +15,11 @@ import matplotlib
 import util
 import activity_features
 
-def visualize(filename, show=True, M10_view=False, **kwargs):
+def visualize(filename, show=True, M10_view=False, temperature=False, **kwargs):
     filename = pathlib.Path(filename)
 
     # Get activity features to plot the 'main sleep' times
     data, results, by_day  = activity_features.run(filename, None)
-
-    mask = numpy.ones(data.index.shape).astype(bool)
-
-    grouped = data[mask].groupby(data.index.time)
-    day_max = grouped.max()
-    day_min = grouped.min()
-    day_avg = grouped.mean()
 
     #### Make figures
     activities = ['main_sleep', 'other_sleep', 'sedentary', 'walking', 'tasks_light', 'moderate', 'imputed']
@@ -44,10 +37,24 @@ def visualize(filename, show=True, M10_view=False, **kwargs):
         day = data.loc[start:end]
         index = (day.index - day.index[0].normalize()) / pandas.to_timedelta('1H')
 
-        # Shade regions based off the inferred activity
+        if not temperature:
+            # Plot acceleration
+            ax.plot(index.values, numpy.log10(day.acceleration.values + 1), c='k')
+
+            # Plot cosinor fit
+            #cosinor = numpy.cos((index - results['phase']) * 2 * numpy.pi / 24) * results['amplitude'] + results['mesor']
+            #ax.plot(index.values, numpy.log10(cosinor + 1), c='k', linestyle="--")
+            cosinor = numpy.cos((index - results['phase']) * 2 * numpy.pi / 24) * results['amplitude'] + results['mesor']
+            ax.plot(index.values, cosinor, c='k', linestyle="--")
+        else:
+            # Plot temperatures
+            ax.plot(index.values, day.temp.values, c='k')
+            # Cosinor tempereature fit
+            cosinor = numpy.cos((index - results['temp_phase']) * 2 * numpy.pi / 24) * results['temp_amplitude'] + results['temp_mesor']
+            ax.plot(index.values, cosinor, c='grey', linestyle="--")
+
         if not M10_view:
-            shading_rects = []
-            shading_activities = []
+            # Shade regions based off the inferred activity
             for act, color in zip(activities, activity_colors):
                 changes = numpy.diff(numpy.concatenate([[0], (day[act] > 0).astype(int), [0]]))
                 starts, = numpy.where(changes > 0)
@@ -58,25 +65,16 @@ def visualize(filename, show=True, M10_view=False, **kwargs):
                     last = index[min(b, len(day.index)-1)]
                     rect = ax.axvspan(first, last, facecolor=color, alpha=0.5)
 
-        # Plot acceleration
-        ax.plot(index.values, numpy.log10(day.acceleration.values + 1), c='k')
-
-        # Plot cosinor fit
-        #cosinor = numpy.cos((index - results['phase']) * 2 * numpy.pi / 24) * results['amplitude'] + results['mesor']
-        #ax.plot(index.values, numpy.log10(cosinor + 1), c='k', linestyle="--")
-        cosinor = numpy.cos((index - results['phase']) * 2 * numpy.pi / 24) * results['amplitude'] + results['mesor']
-        ax.plot(index.values, cosinor, c='k', linestyle="--")
-
-        if not M10_view:
             # Plot light
             ax_light = ax.twinx()
             ax_light.plot(index.values, day.light.values, c='w')
             ax_light.set_yticks([],[])
+            ax_light.set_yticks([],minor=[])
             ax_temp = ax.twinx()
             ax_temp.plot(index.values, day.temp.values, c='grey')
-            ax_temp.set_yticks([],[])
+            ax_temp.set_yticks([],minor=[])
 
-            ax.set_yticks([],[])
+            ax.set_yticks([],minor=[])
             ax.set_ylabel(start.strftime("%a") + "\n" + start.strftime("%y-%m-%d"))
             ax.set_xticks([0,6,12,18,24,30,36,42,48])
             ax.set_xlim([0,48])
@@ -116,7 +114,7 @@ def visualize(filename, show=True, M10_view=False, **kwargs):
                 ax.axvline(L5_start + i, **hour_plots_args)
                 ax.axvline(L5_start + i + 24, **hour_plots_args)
 
-            ax.set_yticks([],[])
+            ax.set_yticks([],minor=[])
             #ax.set_ylabel(start.strftime("%a") + "\n" + start.strftime("%y-%m-%d")) # With dates
             ax.set_ylabel(start.strftime("%a")) # Just day-of-week
             ax.set_xticks([0,4,8,12,16,20,24])
@@ -125,6 +123,7 @@ def visualize(filename, show=True, M10_view=False, **kwargs):
 
     axes[0].set_title(filename.name)
 
+    # Legends
     if not M10_view:
         painters = [matplotlib.patches.Patch(color=color, alpha=0.5, label=act) for color, act in zip(activity_colors, activities)]
         painters.append(matplotlib.lines.Line2D([0],[0], color="k", label="accel"))
@@ -132,19 +131,20 @@ def visualize(filename, show=True, M10_view=False, **kwargs):
         painters.append(matplotlib.lines.Line2D([0],[0], color="grey", label="temp"))
         fig.legend(painters, activities + ['accel', 'light', 'temp'])
     else:
+        label = "accel" if not temperature else "temp"
         painters = [matplotlib.patches.Patch(color=M10_color, label="M10"),
                     matplotlib.patches.Patch(color=L5_color, label="L5"),]
-        painters.append(matplotlib.lines.Line2D([0],[0], color="k", label="accel"))
-        fig.legend(painters, ['M10', 'L5', 'accel'])
+        painters.append(matplotlib.lines.Line2D([0],[0], color="k", label=label))
+        fig.legend(painters, ['M10', 'L5', label])
 
-        # Annotations
-        ax.annotate("Between-day SD",
-                    xy = (0.9,0.2), xycoords="figure fraction",
-                    xytext=(10,0), textcoords="offset pixels",
-                    verticalalignment='center',
-                    arrowprops={
-                        'arrowstyle': '-[, widthB=2.0',
-                    })
+        ## Annotations
+        #ax.annotate("Between-day SD",
+        #            xy = (0.9,0.2), xycoords="figure fraction",
+        #            xytext=(10,0), textcoords="offset pixels",
+        #            verticalalignment='center',
+        #            arrowprops={
+        #                'arrowstyle': '-[, widthB=2.0',
+        #            })
 
 
     # 24Hour compressed data
@@ -165,8 +165,9 @@ def visualize(filename, show=True, M10_view=False, **kwargs):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=f"Visualize an actigraphy data, preferably downsampled to be, say, 1minute intervals")
     parser.add_argument("-f", "--filename", required=True, help="path(s) to file(s) to load", nargs="+")
-    parser.add_argument("--M10_view", required=False, default=False, action="store_const", const=True, dest="M10_view")
+    parser.add_argument("--temperature", required=False, default=False, action="store_const", const=True, dest="temperature", help="Emphasize temperature not activity")
+    parser.add_argument("--M10_view", required=False, default=False, action="store_const", const=True, dest="M10_view", help="Highlight M10/L5 areas")
     args = parser.parse_args()
 
     for filename in args.filename:
-        data, results = visualize(filename, M10_view=args.M10_view)
+        data, results = visualize(filename, M10_view=args.M10_view, temperature=args.temperature)
